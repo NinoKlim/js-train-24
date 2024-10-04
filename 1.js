03cc1765c71609579e0f6b97251ea360f257abac
NinoKlim <nnikitka@gmail.com>
2024-10-04 14:24:11 +0200

    js train Promises

--- 1.js
+++ 1.js
@@ -9,6 +9,14 @@ function task1() {
    *  timeout - час в мілісекундах, який витрачається на отримання даних,за замовчуванням 2000мс.
    */
   function getData(data, timeout = 2000) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Object.keys(data).length > 0) {
+          resolve(data);
+        }
+        reject(new Error("Об'єкт пустий"));
+      }, timeout);
+    });
     // Функція повертає новий проміс
     // За допомогою setTimeout ми симулюємо затримку timeout, яка виникає при роботі з асинхронними джерелами даних
     // Якщо об'єкт не пустий, ми викликаємо resolve з data
@@ -16,7 +24,16 @@ function task1() {
   }
 
   // Ми викликаємо getData з об'єктом { name: "John", age: 30, city: "New York" } і часом очікування 2000
-
+  getData({ name: "John", age: 30, city: "New York" })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    })
+    .finally(() => {
+      console.log("Завдання 1 завершено");
+    });
   // Ми обробляємо дані, повернуті промісом
   // У разі успіху виводимо в консоль дані
   // У разі помилки виводимо повідомлення помилки
--- 8.js
+++ 8.js
@@ -2,10 +2,22 @@ console.log("Завдання: 8 ==============================");
 
 function task8() {
   // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
-  // Створюємо fakeData константу з данними { name: "John",age: 30}
-  // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
+  const fetchFakeData = () => {
+    // Створюємо fakeData константу з данними { name: "John",age: 30}
+    // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
+    const fakeData = { name: "John", age: 30 };
+    return Promise.resolve(fakeData);
+  };
   // Викликаємо функцію fetchFakeData
+  fetchFakeData()
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
+
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
 }
 
